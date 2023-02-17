6f4bf8f4cb7529ba35ce6fe7f413a127fe0ca49d
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-02-17 15:47:40 -0300

    Added dark theme to the website (light or dark theme is activated according to the user's system preference)

--- web/src/layouts/Default.tsx
+++ web/src/layouts/Default.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import { Outlet } from "react-router-dom";
+import useLocalStorage from 'use-local-storage'
 import { Sidebar } from '../components/Sidebar'
 import '../styles/default-layout.css'
 
 export function Default() {
+	const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const [theme, setTheme] = useState(isDarkTheme ? 'dark' : 'light')
+
 	return (
-		<div className="layout">
+		<div className="layout" data-theme={theme}>
 			<Sidebar />
 			<div className="content">
 				<Outlet />
